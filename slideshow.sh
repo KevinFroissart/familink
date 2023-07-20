40dca5364ba80568ebc320f0a03e1136abdf21ca
KevinFroissart <kevin.froissart69@gmail.com>
2023-07-20 19:13:50 +0200

    dynamic reload

--- slideshow.sh
+++ slideshow.sh
@@ -3,9 +3,22 @@
 # Répertoire contenant les images
 IMAGE_DIR="/home/kfroissart/git/familink/images/"
 
+# Fonction pour compter le nombre d'images dans le répertoire
+count_images() {
+  local count=0
+  for file in "$1"/*; do
+    if [[ -f "$file" ]]; then
+      ((count++))
+    fi
+  done
+  echo "$count"
+}
+
 # Boucle infinie pour exécuter le diaporama en continu
 while true; do
-  feh --quiet --full-screen --hide-pointer --reload 1300 --slideshow-delay 10 "$IMAGE_DIR"
+  image_count=$(count_images "$IMAGE_DIR")
+  reload_time=$((image_count * 10 + 20))
+  feh --quiet --full-screen --hide-pointer --reload "$reload_time" --slideshow-delay 10 "$IMAGE_DIR"
 done
 
 # Redémarrer le script pour relancer le diaporama
